30b070997f4f8a2eed6e8571bc8de452816303b7
jadilovic <adilovic79@yahoo.com>
2022-12-02 16:07:14 +0100

    up to routing

--- app.vue
+++ app.vue
@@ -1,5 +1,34 @@
 <template>
-  <div>
-    Hello World!
-  </div>
+  <!-- <home-welcome /> -->
+  <!-- <button @click="toggle">Toggle</button> -->
+  <NuxtPage />
+  <br>
+  <a href="#" @click="view('welcome')">Welcome</a>
+  <br>
+  <a href="#" @click="view('counter')">Counter</a>
+  <br>
+  <!-- <div v-if="MyComponent === 'welcome'">
+    <home-welcome />
+  </div> -->
+  <lazy-home-welcome v-if="MyComponent === 'welcome'" />
+  <br>
+  <lazy-counter v-if="MyComponent === 'counter'" />
+  <!-- <component :is="MyComponent" /> -->
+  <!-- <div v-else>
+    <counter />
+  </div> -->
 </template>
+
+<script setup>
+  const MyComponent = ref("counter");
+
+  function view(name) {
+    MyComponent.value = name;
+  }
+
+  function toggle() {
+    console.log(MyComponent.value);
+    // MyComponent.value = resolveComponent("home-welcome")
+     MyComponent.value = "welcome";
+  }
+</script>
